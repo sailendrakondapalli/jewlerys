d33ae08d28ff6d44693b0435ecd7e7fef68816ce
Asshu-06 <adduriaswani@gmail.com>
2026-07-27 19:25:25 +0530

    Update UPI ID to q901588902@ybl

--- src/pages/CheckoutPage.jsx
+++ src/pages/CheckoutPage.jsx
@@ -11,7 +11,7 @@ import { supabase } from "../lib/supabase"
 import { formatINR } from "../utils/format"
 import toast from "react-hot-toast"
 
-const UPI_ID = "Q487529392@ybl"
+const UPI_ID = "q901588902@ybl"
 const ADMIN_WHATSAPP = "918639006849"
 // Generate QR dynamically from UPI ID using Google Charts API
 const getQRUrl = (upiId, amount) =>
